20e7faf57bcbd1a8312c93d6a9759e72688cc5ff
DaviGel <geloso.davide@gmail.com>
2023-11-07 16:54:12 +0100

    feat: milestone1 in progress

--- js/main.js
+++ js/main.js
@@ -18,30 +18,30 @@ const myButton = document.querySelector('button[type=submit]');
 myButton.addEventListener('click', function () {
   console.log(kilometri.value);
   console.log(etaPasseggero.value);
-});
 
-// Condizione
-if (
-  !isNaN(kilometri) &&
-  !isNaN(etaPasseggero) &&
-  kilometri > 0 &&
-  etaPasseggero > 0 &&
-  etaPasseggero <= 130
-) {
-  let prezzoBiglietto = kilometri * prezzoKilometro;
+  // Condizione
+  if (
+    !isNaN(kilometri) &&
+    !isNaN(etaPasseggero) &&
+    kilometri > 0 &&
+    etaPasseggero > 0 &&
+    etaPasseggero <= 130
+  ) {
+    let prezzoBiglietto = kilometri * prezzoKilometro;
 
-  if (etaPasseggero < 18) {
-    prezzoBiglietto -= (prezzoBiglietto * scontoMinori) / 100;
-  } else if (etaPasseggero > 65) {
-    prezzoBiglietto -= (prezzoBiglietto * scontoAnziani) / 100;
+    if (etaPasseggero < 18) {
+      prezzoBiglietto -= (prezzoBiglietto * scontoMinori) / 100;
+    } else if (etaPasseggero > 65) {
+      prezzoBiglietto -= (prezzoBiglietto * scontoAnziani) / 100;
+    }
+
+    /* N.B. dato che il metodo toFixed() restituisce una stringa, potrei convertire prezzoBiglietto.toFixed(2) in un numero, qualora fosse necessario, tramite la funzione Number() */
+    console.log(
+      'Il prezzo del biglietto è:',
+      Number(prezzoBiglietto.toFixed(2)),
+      '€'
+    );
+  } else {
+    console.log("Inserire i km validi e un'età valida in formato numerico!");
   }
-
-  /* N.B. dato che il metodo toFixed() restituisce una stringa, potrei convertire prezzoBiglietto.toFixed(2) in un numero, qualora fosse necessario, tramite la funzione Number() */
-  console.log(
-    'Il prezzo del biglietto è:',
-    Number(prezzoBiglietto.toFixed(2)),
-    '€'
-  );
-} else {
-  console.log("Inserire i km validi e un'età valida in formato numerico!");
-}
+});
