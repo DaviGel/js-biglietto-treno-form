27c19690dfbce51e32572fc7b71c416fd0517057
DaviGel <geloso.davide@gmail.com>
2023-11-07 22:46:07 +0100

    feat: milestone2

--- js/main.js
+++ js/main.js
@@ -5,41 +5,63 @@ const scontoMinori = 20;
 const scontoAnziani = 40;
 const prezzoKilometro = 0.21;
 
-// Kilometri
-let kilometri = document.getElementById('kilometri');
+// Bottone genera
+const bottoneGenera = document.querySelector('button[type=button]');
 
-// Età
-let etaPasseggero = document.getElementById('eta-passeggero');
+// Bottone annulla
+const bottoneAnnulla = document.querySelector('button[type=reset]');
 
-// Nome e cognome
-let nomePasseggero = document.getElementById('nome-passeggero');
+bottoneGenera.addEventListener('click', function () {
+  // Kilometri
+  let kilometri = document.getElementById('kilometri').value;
 
-// Bottone
-const myButton = document.querySelector('button[type=button]');
+  // Nome e cognome
+  let nomePasseggero = document.getElementById('nome-passeggero').value;
+  document.getElementById('nome-passeggero-biglietto').innerHTML =
+    'Nome passeggero<br />' + nomePasseggero;
 
-myButton.addEventListener('click', function () {
-  kilometri = kilometri.value;
-  etaPasseggero = etaPasseggero.text;
-  console.log(etaPasseggero);
-  nomePasseggero = nomePasseggero.value;
+  // Età
+  let etaPasseggero = document.getElementById('eta-passeggero').value;
+  // Qualora volessi selezionare il testo interno potrei usare questa istruzione. Se scegliessi questa strada nelle condizioni sotto potrei selezionare conforntando con le stringhe "Minorenne" o "Maggiorenne" o "Over 65"
+  // etaPasseggero = etaPasseggero.options[etaPasseggero.selectedIndex].text;
 
-  // Condizione
+  // Prezzo biglietto
+  let prezzoBiglietto = kilometri * prezzoKilometro;
+
+  document.getElementById('costo-biglietto').innerHTML = null;
+
+  // Condizione kilometri validi
   if (!isNaN(kilometri) && kilometri > 0) {
-    let prezzoBiglietto = kilometri * prezzoKilometro;
-
-    if (etaPasseggero === 'Minorenne') {
+    // Condizione età passeggero
+    if (Number(etaPasseggero) === 1) {
       prezzoBiglietto -= (prezzoBiglietto * scontoMinori) / 100;
-    } else if (etaPasseggero === 'Over 65') {
+    } else if (Number(etaPasseggero) === 3) {
       prezzoBiglietto -= (prezzoBiglietto * scontoAnziani) / 100;
     }
-
-    /* N.B. dato che il metodo toFixed() restituisce una stringa, potrei convertire prezzoBiglietto.toFixed(2) in un numero, qualora fosse necessario, tramite la funzione Number() */
+    // Stampa in console
     console.log(
       'Il prezzo del biglietto è:',
       Number(prezzoBiglietto.toFixed(2)),
       '€'
     );
+    // Stampa in pagina
+    document.getElementById('costo-biglietto').innerHTML =
+      Number(prezzoBiglietto.toFixed(2)) + ' ' + '€';
   } else {
-    console.log("Inserire i km validi e un'età valida in formato numerico!");
+    console.log('Inserire i km validi in formato numerico!');
   }
+
+  // Codice Postale
+  const min = 10000;
+  const max = 99999;
+  document.getElementById('cap').innerHTML =
+    Math.floor(Math.random() * (max - min + 1)) + min;
+
+  // Mostra Biglietto
+  document.getElementById('biglietto').classList.remove('d-none');
+});
+
+bottoneAnnulla.addEventListener('click', function () {
+  // Nascondi biglietto
+  document.getElementById('biglietto').classList.add('d-none');
 });
